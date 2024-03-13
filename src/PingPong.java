2ac3a1b9af1178bd3d144ed5df4a97accba9ad61
mikelsv1 <salvoch.140764@e.unavarra.es>
2024-03-13 10:14:26 +0100

    step back

--- src/PingPong.java
+++ src/PingPong.java
@@ -5,8 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PingPong {
-    private static final String INIT_QUEUE_NAME_1 = "init_queue_1";
-    private static final String INIT_QUEUE_NAME_2 = "init_queue_2";
+    private static final String INIT_QUEUE_NAME = "init_queue_1";
     private static final String PING_QUEUE_NAME = "ping_queue";
     private static final String PONG_QUEUE_NAME = "pong_queue";
 
@@ -18,24 +17,19 @@ public class PingPong {
     private static boolean isStarted = false;
     private static String nodeId;
 
-    private static List<Integer> nodeIds = new ArrayList<Integer>();
-
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.queueDeclare(INIT_QUEUE_NAME_1, false, false, false, null);
-        channel.queueDeclare(INIT_QUEUE_NAME_2, false, false, false, null);
+        channel.queueDeclare(INIT_QUEUE_NAME, false, false, false, null);
         channel.queueDeclare(PING_QUEUE_NAME, false, false, false, null);
         channel.queueDeclare(PONG_QUEUE_NAME, false, false, false, null);
 
         nodeId = Integer.toString((int) (Math.random() * 10000));
-        nodeIds.add(Integer.parseInt(nodeId));
 
-        sendMessage(channel, INIT_QUEUE_NAME_1, INIT_MESSAGE + ":" + nodeId);
-        sendMessage(channel, INIT_QUEUE_NAME_2, INIT_MESSAGE + ":" + nodeId);
+        sendMessage(channel, INIT_QUEUE_NAME, INIT_MESSAGE + ":" + nodeId);
 
         DeliverCallback initCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
@@ -43,27 +37,23 @@ public class PingPong {
             String[] parts = message.split(":");
             String receivedId = parts[1];
             if (!isStarted) {
-                if(!nodeIds.contains(Integer.parseInt(receivedId))) {
-                    nodeIds.add(Integer.parseInt(receivedId));
-                }
-                sendMessage(channel, INIT_QUEUE_NAME_1, INIT_MESSAGE + ":" + nodeId);
-                sendMessage(channel, INIT_QUEUE_NAME_2, INIT_MESSAGE + ":" + nodeId);
-                if (receivedId.compareTo(nodeId) < 0) {
-                    sendMessage(channel, INIT_QUEUE_NAME_1, INIT_MESSAGE + ":" + nodeId);
-                    sendMessage(channel, INIT_QUEUE_NAME_2, INIT_MESSAGE + ":" + nodeId);
+                if (receivedId.compareTo(nodeId) <= 0) {
+                    sendMessage(channel, INIT_QUEUE_NAME, INIT_MESSAGE + ":" + nodeId);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 } else if (receivedId.compareTo(nodeId) > 0) {
-                    sendMessage(channel, INIT_QUEUE_NAME_1, OK_MESSAGE);
-                    sendMessage(channel, INIT_QUEUE_NAME_2, OK_MESSAGE);
+                    sendMessage(channel, INIT_QUEUE_NAME, OK_MESSAGE);
                     isStarted = true;
                     sendMessage(channel, PING_QUEUE_NAME, PING_MESSAGE);
                 }
             }
         };
-        channel.basicConsume(INIT_QUEUE_NAME_1, true, initCallback, consumerTag -> {
+        channel.basicConsume(INIT_QUEUE_NAME, true, initCallback, consumerTag -> {
         });
-        channel.basicConsume(INIT_QUEUE_NAME_2, true, initCallback, consumerTag -> {
-        });
-        
+
         DeliverCallback pingCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println("Received: '" + message + "'");
